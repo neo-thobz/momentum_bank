75ef6a58e1f875d0d6206415044b2f63c1036e2b
Neo <neo.thobz@gmail.com>
2020-02-10 14:48:42 +0200

    save changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'movies',
+    loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'movies/:id',
+    loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./public/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./public/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path : 'members',
+    canActivate: [AuthGuardService],
+    loadChildren: () => import('./members/members-routing.module')
+  }
 ];
 
 @NgModule({
